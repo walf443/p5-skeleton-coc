86d536cfcd1d6c2d474ec26a35c583567b0b1456
Keiji, Yoshimi <walf443@gmail.com>
2011-04-09 20:34:06 +0900

    initial commit

---
+++ lib/Skeleton/CoC.pm
@@ -0,0 +1,33 @@
+package Skeleton::CoC;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Skeleton::CoC -
+
+=head1 SYNOPSIS
+
+  use Skeleton::CoC;
+
+=head1 DESCRIPTION
+
+Skeleton::CoC is
+
+=head1 AUTHOR
+
+Keiji Yoshimi E<lt>walf443 at gmail dot comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
